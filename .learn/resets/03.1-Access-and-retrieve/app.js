6e2a5d1add523338ce70f1843401341738b4fec2
ljoseojeda <luis.jose1503@gmail.com>
2021-11-03 20:00:46 +0000

    arreglos practicas

--- .learn/resets/03.1-Access-and-retrieve/app.js
+++ .learn/resets/03.1-Access-and-retrieve/app.js
@@ -3,10 +3,6 @@ var myArray = ['sunday','monday','tuesday','wednesday','thursday','friday','satu
 
 //1. print the item here
 
-console.log(myArray[2]);
-
 //2. change 'thursday'a value here to null
-myArray[4] = null; 
-//3. print the position of step 2
 
-console.log(myArray[4]);
+//3. print the position of step 2
